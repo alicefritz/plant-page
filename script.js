b9775b158f4a8acbd7d2c38b6b4cb248dd42622d
alicefritz <alicejfritz@gmail.com>
2021-02-19 14:29:12 +0100

    added basic 'hamburger'-type menu

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const navToggle = document.querySelector('.nav-toggle');
+
 const slideShow = () => {
   const currentSlide = document.querySelector('.active-slide')
   let nextSlide = currentSlide.nextElementSibling;
@@ -11,3 +13,9 @@ const slideShow = () => {
 setInterval(() => {
   slideShow()
 }, 10000);
+
+navToggle.addEventListener('click', () => {
+  const navElement = document.querySelector('nav');
+  navElement.classList.toggle('nav-open');
+  navToggle.classList.toggle('nav-toggle-open')
+})
